31ec1149300fb6695980c2be76aec383831f4db3
AHSAN <ahsan.bsse3200@iiu.edu.pk>
2022-03-26 22:45:05 +0500

    Initial commit

---
+++ app/src/main/java/com/info/coinappwithcleanarch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.info.coinappwithcleanarch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coinappwithcleanarch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
